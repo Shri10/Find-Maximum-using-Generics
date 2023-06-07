cc4a58a5296fde60b5c1180c9438ca88422dcadc
Shri10 <pethe.shriraj@gmail.com>
2023-06-07 10:34:40 +0530

    UC2 Created

--- Find Maximum using Generics/Program.cs
+++ Find Maximum using Generics/Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Maximum of 5, 3, 7:\t" + FindMax(5, 3, 7)); //7
-            Console.WriteLine("Maximum of 1, 9, 2:\t" + FindMax(1, 9, 2)); //9
-            Console.WriteLine("Maximum of 6, 4, 5:\t" + FindMax(6, 4, 5)); //6
-            Console.WriteLine("Maximum of 99, 0, -5:\t" + FindMax(99, 0, -5)); //99
+            Console.WriteLine("Maximum of 5.7, 3.1, 7.9:\t" + FindMax(5.7f, 3.1f, 7.9f)); //7.9
+            Console.WriteLine("Maximum of 1.2, 9.5, 2.6:\t" + FindMax(1.2f, 9.5f, 2.6f)); //9.5
+            Console.WriteLine("Maximum of 6.0, 4.3, 5.8:\t" + FindMax(6.0f, 4.3f, 5.8f)); //6.0
+            Console.WriteLine("Maximum of 99.9, 0.0, -5.5:\t" + FindMax(99.9f, 0.0f, -5.5f)); //99
         }
 
-        public static int FindMax(int a, int b, int c)
+        public static float FindMax(float a, float b, float c)
         {
-            int max = a;
+            float max = a;
 
             if (b.CompareTo(max) > 0)
             {
